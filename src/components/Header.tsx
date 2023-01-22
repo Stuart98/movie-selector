0e2f69c3f8496fad0c42e8acdd2a18c90314c5c6
Stuart Ashworth <stuart@stuartashworth.com>
2023-01-22 20:36:54 +0000

    Titles and tidyups

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,8 @@ import { Heading } from "@chakra-ui/react";
 export default function Header() {
   return (
     <header className="main-header">
-      <Heading as="h1" size="lg">Movie Shortlist</Heading>
+      <Heading as="h1" size="lg">Movie Shortlister</Heading>
+      <Heading as="h2" size="sm">Create shortlists of movies and TV shows for your kids to choose</Heading>
     </header>
   );
 }
